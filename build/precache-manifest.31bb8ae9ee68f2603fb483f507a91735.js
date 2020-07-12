e0b595513e58bc3cab83634628f561269e84fb0d
Richard Peters <richard@dev.ngo>
2020-07-12 20:28:14 +0100

    Build files

---
+++ build/precache-manifest.31bb8ae9ee68f2603fb483f507a91735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393d9d67551a44ecf299bd37fc518566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7598d2cd9c224554e81",
+    "url": "/static/css/main.20f2a005.chunk.css"
+  },
+  {
+    "revision": "1e19e074f4533ba5a604",
+    "url": "/static/js/2.aa60b873.chunk.js"
+  },
+  {
+    "revision": "0fd3b68224160b005b25f9190a87c582",
+    "url": "/static/js/2.aa60b873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7598d2cd9c224554e81",
+    "url": "/static/js/main.9b1cdd74.chunk.js"
+  },
+  {
+    "revision": "02c1cf649ee0b6636c49",
+    "url": "/static/js/runtime-main.37433cb9.js"
+  },
+  {
+    "revision": "90cd3d0faf48982daccd1bad8ba4935d",
+    "url": "/static/media/loading.90cd3d0f.svg"
+  }
+]);
