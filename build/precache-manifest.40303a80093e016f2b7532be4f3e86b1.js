b03d3cb16b6b000a7329c8a67a8fe0a37d269222
Richard Peters <richard@dev.ngo>
2020-07-13 08:49:22 +0100

    Static files

---
+++ build/precache-manifest.40303a80093e016f2b7532be4f3e86b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a82241069d79c88d34aed5d6f4b5493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfbba0175e5c2d078fcc",
+    "url": "/static/css/main.b36b6ef5.chunk.css"
+  },
+  {
+    "revision": "1e19e074f4533ba5a604",
+    "url": "/static/js/2.aa60b873.chunk.js"
+  },
+  {
+    "revision": "0fd3b68224160b005b25f9190a87c582",
+    "url": "/static/js/2.aa60b873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbba0175e5c2d078fcc",
+    "url": "/static/js/main.ad5138ef.chunk.js"
+  },
+  {
+    "revision": "02c1cf649ee0b6636c49",
+    "url": "/static/js/runtime-main.37433cb9.js"
+  },
+  {
+    "revision": "90cd3d0faf48982daccd1bad8ba4935d",
+    "url": "/static/media/loading.90cd3d0f.svg"
+  }
+]);
